e98bd013dc64edb1279c460cc39e2c3bcab4023b
Adotac <otacool89@gmail.com>
2022-04-24 00:13:16 +0800

    spyLog implement

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -4,49 +4,80 @@ import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
 
 fdescribe('CalculatorService - Montero, Joshua', () => {
-  let service: CalculatorService;
+  let calculator: CalculatorService;
+  let logSpy: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CalculatorService);
+    logSpy = jasmine.createSpyObj('LoggerService', ['log', 'errorMessage', 'warn']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,{
+          provide: LoggerService,
+          useValue: logSpy,
+        }
+      ],
+
+    });
+    calculator = TestBed.inject(CalculatorService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(calculator).toBeTruthy();
   });
 
+  //-------------------------------------//
+  
   it('should add 2 numbers', () => {
-    let calculator = new CalculatorService(new LoggerService());
     let result = calculator.add(5, 5);
-
+    expect(logSpy.log).toHaveBeenCalledWith('Addition operation called');
     expect(result).toBe(10);
-  })
+  });
 
   it('should subtract 2 numbers', () => {
-    let calculator = new CalculatorService(new LoggerService());
     let result = calculator.subtract(5, 5);
-
+    expect(logSpy.log).toHaveBeenCalledWith('Subtraction operation called');
     expect(result).toBe(0);
-  })
+  });
 
-  it('should multiply 2 numbers', () => {
-    let calculator = new CalculatorService(new LoggerService());
-    let result = calculator.multiply(5, 5);
-
+  it('should return a positive value when multiplying 2 numbers', () => {
+   let result = calculator.multiply(5, 5);
+    expect(logSpy.log).toHaveBeenCalledWith('Multiplication operation called');
+    expect(logSpy.log).toHaveBeenCalledWith('Result is positive');
     expect(result).toBe(25);
-  })
+  });
+
+  it('should return a negative value when multiplying 2 numbers', () => {
+    let result = calculator.multiply(-5, 5);
+     expect(logSpy.log).toHaveBeenCalledWith('Multiplication operation called');
+     expect(logSpy.warn).toHaveBeenCalledWith('Result is negative');
+     expect(result).toBe(-25);
+   });
 
   it('should divide 2 numbers', () => {
-    let calculator = new CalculatorService(new LoggerService());
-    //let result1 = calculator.divide(5, 0);
-    let result2 = calculator.divide(25, 5);
+    let result = calculator.divide(25, 5);
+    expect(logSpy.log).toHaveBeenCalledWith('Division operation called');
+    expect(result).toBe(5);
+  });
 
+  it('should not divide by zero', () => {
+    let result = calculator.divide(25, 0);
+    expect(logSpy.errorMessage).toHaveBeenCalledWith('Check divide function -- cannot divide by 0');
+    expect(result).toBe("Cannot divide by 0");
+  });
 
-    expect(function(){
-      calculator.divide(5, 0)
-    }).toThrow();
-    
-    expect(result2).toBe(5);
-
-  })
+  it('should return 1 if testfunction called with 1', () => {
+    let result = calculator.testFunction(1);
+    expect(result).toBe(1);
+  });
+  it('should return 2 if testfunction called with 2', () => {
+    let result = calculator.testFunction(2);
+    expect(result).toBe(2); 
+  });
+  it('should return 3 if testfunction called with any number, except for 1 and 2', () => {
+    let result = calculator.testFunction(21);
+    expect(result).not.toBe(1);
+    expect(result).not.toBe(2);
+    expect(result).toBe(3);
+  });
 });
--- src/app/services/logger.service.ts
+++ src/app/services/logger.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+ 
 @Injectable({
   providedIn: 'root',
 })
@@ -6,4 +7,13 @@ export class LoggerService {
   log(message: string) {
     console.log(message);
   }
+ 
+  warn(message: string) {
+    console.warn(message);
+  }
+ 
+  errorMessage(message: string) {
+    console.error(message);
+  }
 }
+ 
